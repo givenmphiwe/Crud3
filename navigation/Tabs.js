2040ad5330d880917e414280a1f176ec7476af95
Given Makhubela <giggslive52@gmail.com>
2022-04-25 16:23:02 +0200

    crud

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,20 +1,25 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LandingScreens from "../screens/LandingScreens";
-
+import AddScreen from "../screens/AddScreen";
 
 const Stack = createNativeStackNavigator();
 
 const Stacks = () => {
   return (
     <Stack.Navigator>
+
       <Stack.Screen
         name="Landing"
         component={LandingScreens}
-        options={{ title: "Login", header: () => null }}
+        options={{ title: "Landing", header: () => null }}
       />
 
-    
+      <Stack.Screen
+        name="AddScreen"
+        component={AddScreen}
+        options={{ title: "AddScreen", header: () => null }}
+      />
     </Stack.Navigator>
   );
 };
